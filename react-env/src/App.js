47e7269fd0cadb184a6aae4835918934945efe73
your.user.name <harmanmalht777@gmail.com>
2023-10-13 19:09:22 +0530

    Added MainSection

--- react-env/src/App.js
+++ react-env/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
 import HeroSection from './components/HeroSection';
+import MainBody from './components/MainBody';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
        <HeroSection/>
+       <MainBody/>
     </div>
   );
 }
