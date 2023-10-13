f4e7b6cb2222a62f75f4c6b727c33207353a7430
your.user.name <harmanmalht777@gmail.com>
2023-10-13 19:25:42 +0530

    [Fix] : Formatation for the Home Page - COMPLETE

---
+++ react-env/src/components/CopyrightFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CopyrightFooter() {
+  return (
+    <div class="container-fluid p-3 text-white text-center" style={{"background-color" : "rgb(29, 29, 29)"}}>
+        <h6>COPYRIGHT@AMITTRIPATHI2023</h6>
+    </div>
+  )
+}
+
+export default CopyrightFooter
