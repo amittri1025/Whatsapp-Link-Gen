6c76e3c07d76fd02dc5a0749b1a35007cb64a99f
your.user.name <harmanmalht777@gmail.com>
2023-10-16 22:58:04 +0530

    Formatted the application

--- react-env/src/components/ContactNavbar.js
+++ react-env/src/components/ContactNavbar.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const ContactNavbar = () => {
   return (
     <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/" >WaLinkGen</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="/">
+            WaLinkGen
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                {/*                 
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              {/*                 
                 <!-- <li class="nav-item">
                     <a class="nav-link" href="#">Blog</a>
                 </li>
@@ -26,13 +36,13 @@ const ContactNavbar = () => {
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Sign-up</a>
-                </li> --> */}    
+                </li> --> */}
             </ul>
+          </div>
         </div>
-    </div>
-</nav>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default ContactNavbar
+export default ContactNavbar;
--- react-env/src/components/Navbar.js
+++ react-env/src/components/Navbar.js
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function Navbar(props) {
-    const changeDarkMode = () => {
-        props.dark === 'dark' ? props.changeDarker('light') : props.changeDarker('dark');
-    } 
+  const changeDarkMode = () => {
+    props.dark === "dark"
+      ? props.changeDarker("light")
+      : props.changeDarker("dark");
+  };
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/" >WaLinkGen</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          WaLinkGen
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-{/*                 
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            {/*                 
                 <!-- <li class="nav-item">
                     <a class="nav-link" href="#">Blog</a>
                 </li>
@@ -29,14 +41,21 @@ function Navbar(props) {
                 <li class="nav-item">
                     <a class="nav-link" href="#">Sign-up</a>
                 </li> --> */}
-                <button class="btn btn-outline-success col-1 col-lg-5 d-flex justify-content-center align-items-center rounded-circle" onClick={changeDarkMode}>
-                  {(props.dark === "dark")? <i class="fa-regular fa-sun"></i>: <i class="fa-solid fa-moon"></i>}
-                </button>
-            </ul>
+            <button
+              class="btn btn-outline-success col-1 col-lg-5 d-flex justify-content-center align-items-center rounded-circle"
+              onClick={changeDarkMode}
+            >
+              {props.dark === "dark" ? (
+                <i class="fa-regular fa-sun"></i>
+              ) : (
+                <i class="fa-solid fa-moon"></i>
+              )}
+            </button>
+          </ul>
         </div>
-    </div>
-</nav>
-  )
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
